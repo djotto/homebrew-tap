82c14619d508f61ed28f97cf2a929980bf180f2f
David Otton <david@otton.org>
2024-06-23 22:29:20 +0100

    Update djotto-scripts to tag v1.0.3
    @todo Github action to update script when a new tag is published

--- Formula/djotto-scripts.rb
+++ Formula/djotto-scripts.rb
@@ -1,20 +1,11 @@
 class DjottoScripts < Formula
   desc "A collection of useful scripts"
   homepage "https://github.com/djotto/scripts"
+  url "https://github.com/djotto/scripts/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "48472a458d1c63a24a49e3009c8fd0c0a83af9d298e84e9ebcafd77a95e6f473"
   head "https://github.com/djotto/scripts.git", using: :git
 
-  # Use a livecheck block to fetch the latest tag for versioning
-  livecheck do
-    url :head
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
   def install
-    # Fetch the latest tag
-    latest_tag = `git describe --tags $(git rev-list --tags --max-count=1)`.strip
-    odie "Unable to fetch latest tag" if latest_tag.empty?
-    version latest_tag
-
     # Install the scripts
     bin.install "git-scripts/git-info"
     bin.install "git-scripts/git-seek"
@@ -22,11 +13,10 @@ class DjottoScripts < Formula
 
   test do
     # Run a simple test to verify the installation of git-info
-    output = shell_output("#{bin}/git-info")
-    assert_match "Not a git repository.", outputa
+    output = shell_output("#{bin}/git-info", 1)
+    assert_match "Not a git repository.", output
     # Run a simple test to verify the installation of git-seek
     output = shell_output("#{bin}/git-seek", 1)
     assert_match "Usage: git seek <pattern>", output
   end
 end
-
