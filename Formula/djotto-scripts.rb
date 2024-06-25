f47a9d2a95024da88d58f7d5fccf361580be8c8b
David Otton <david@otton.org>
2024-06-25 12:05:48 +0100

    djotto-scripts v1.0.4 -> v1.0.5

--- Formula/djotto-scripts.rb
+++ Formula/djotto-scripts.rb
@@ -1,8 +1,8 @@
 class DjottoScripts < Formula
   desc "A collection of useful scripts"
   homepage "https://github.com/djotto/scripts"
-  url "https://github.com/djotto/scripts/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "99af5ca308b424fe4422946e5f0e964816a79268519fa9d75315f830e3819ea3"
+  url "https://github.com/djotto/scripts/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "04231b22e8126aa18b8a5cff4423261c151590765f1a4cc22c1311b80f0ef854"
   head "https://github.com/djotto/scripts.git", using: :git
 
   def install
